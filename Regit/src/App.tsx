f4287d468192d8aad6b9a5da5d036e11cca6cc7d
Jamjamzz <codejamzz27@gmail.com>
2025-05-15 07:35:30 +0800

    regit-routes

---
+++ Regit/src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import Header from "./Components/Project_BodyPages/Header.tsx";
+
+function App() {
+  return (
+    <>
+      <Header title="Regit"></Header>
+    </>
+  );
+}
+
+export default App;
---
+++ Regit/src/Components/Project_BodyPages/Header.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout.tsx";
+import "./Page_CSS/header.css";
+import Home from "./Home.tsx";
+import About from "./About.tsx";
+import Contact from "./Contact.tsx";
+
+interface Props {
+  title: String;
+}
+
+export default function Header({ title }: Props) {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout title={title} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
